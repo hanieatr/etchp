98161e64c42877c58f8f934d50e8336b4333eca8
hanieatr <aaatrhanie@gmail.com>
2024-08-17 14:04:00 +0330

    app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import pickle
